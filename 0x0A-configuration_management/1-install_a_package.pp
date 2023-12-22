1ea5b977c433d45c9b80942721cf8e4b9d1dda1a
Mahmoud1yasser <Mahmoudyasser7388@yahoo.com>
2023-12-22 11:54:07 -0800

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -10,11 +10,11 @@ package { 'python3-pip':
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip',
-  require  => Package[python3-pip']
+  require  => Package['python3-pip']
 }
 
 package { 'werkzeug':
   ensure   => '2.1.1',
   provider => 'pip',
-  require  => Package[python3-pip'],
+  require  => Package['python3-pip'],
 }
