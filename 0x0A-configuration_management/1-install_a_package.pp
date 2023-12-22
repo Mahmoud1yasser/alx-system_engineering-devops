f32250e755354c8785948350898e8cc93bd3728f
Mahmoud1yasser <Mahmoudyasser7388@yahoo.com>
2023-12-22 10:59:58 -0800

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
 #Using Puppet, install flask from pip3
-exec { 'install_flask':
-  command     => '/usr/bin/pip3 install Flask==2.1.0',
-  path        => ['/usr/bin', '/usr/local/bin'],
+package { 'Flask':
+  name     => 'flask',
+  ensure   => '2.1.0',
+  provider => 'pip3',
   }
