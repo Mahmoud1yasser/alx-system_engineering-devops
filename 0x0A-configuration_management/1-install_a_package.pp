2c3ba6868f9dae68bd88b73c827a0ac752227bd1
Mahmoud1yasser <Mahmoudyasser7388@yahoo.com>
2023-12-22 11:16:34 -0800

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 #install a flask from pip3
+#This puppet mainfest installs flask from pip3
+package { 'python3-pip':
+  ensure => 'installed',
+}
+
 exec { 'install flask':
-  command =>  'pip3 install flask==2.1.0',
-  path    =>  '/usr/bin/',
-  unless  =>  'pip3 list | grep flask',
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => ['/usr/bin'],
+  unless  => '/usr/bin/pip3 freeze | /bin/grep Flask | /bin/grep 2.1.0',
 }
